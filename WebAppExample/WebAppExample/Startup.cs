65a4d8b47dc1e15d66aaf96439adc8c89f62aba3
Eugene Park <kr361@naver.com>
2016-02-15 13:59:19 +1300

    NotififcationHubAPI

---
+++ WebAppExample/WebAppExample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAppExample.Startup))]
+
+namespace WebAppExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}

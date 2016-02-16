b817c10bd132695ae2e06dcd19bcba5a04eb9760
Charles Lan <charlis.lan@gmail.com>
2016-02-17 11:51:14 +1300

    Added NotificationAPIFinal - initial base.

---
+++ NotificationAPIFinal/NotificationAPIFinal/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NotificationAPIFinal.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
--- NotificationSignalR/NotificationSignalR/Models/User.cs
+++ NotificationSignalR/NotificationSignalR/Models/User.cs
@@ -11,7 +11,6 @@ namespace NotificationSignalR.Models
         [Key]
         public int UserID { get; set; }
         public string UserName { get; set; }
-        public ICollection<Connection> Connections { get; set; }
 
         public ICollection<UserNotification> UserNotifications { get; set; }
     }
